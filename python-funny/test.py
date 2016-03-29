6328803a8c644144102ee1eb22e52ea0438a5c16
Jingkai Tang <jingkaitang@gmail.com>
2016-03-29 10:36:59 +0800

    add: waste time loop

--- python-funny/test.py
+++ python-funny/test.py
@@ -1,6 +1,15 @@
 #! /usr/bin/env python
 
+def loop(n):
+    x = 0
+    for i in range(n):
+        for j in range(i):
+            for k in range(j):
+                x += k
+    return x
+
 if __name__ == '__main__':
     print('hello, world')
     print('have a nice day!')
+    print(loop(1000))
 
